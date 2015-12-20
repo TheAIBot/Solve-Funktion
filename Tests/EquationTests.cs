81c8a9be4b39e7ca8eb19ead6b02d2cb6ed23524
TheAIBot <andreasuranus@hotmail.com>
2015-12-20 04:13:53 +0100

    extended equationTests

--- Tests/EquationTests.cs
+++ Tests/EquationTests.cs
@@ -10,11 +10,11 @@ namespace Tests
     public class EquationTests
     {
         [TestMethod]
-        public void TestEquation()
+        public void TestEquation1()
         {
             Equation e1 = TestTools.MakeEquation("x = {3, 4, 5, 6}", "4, 6, 8, 10");
 
-            // x = x * x
+            // (x) = (x) * x
             Operator o1 = e1.OPStorage.Pop();
             o1.ContainedList = e1.EquationParts;
             e1.AllOperators.Add(o1);
@@ -26,7 +26,7 @@ namespace Tests
             o1.parameterIndex = 0;
             o1.ExtraMathFunction = new Multiply();
 
-            // - 1 = (x - 1) * 2
+            // - 1 = (x - 1) * x
             Operator o2 = e1.OPStorage.Pop();
             o2.ContainedList = o1.Operators;
             e1.AllOperators.Add(o2);
@@ -48,6 +48,31 @@ namespace Tests
             o1.ResultOnRightSide = false;
             o2.ResultOnRightSide = true;
             CheckEquationShowResult(e1, e1.CreateFunction(), "f(x) = ((1 - x) * x)", new double[] { -6, -12, -20, -30 });
+
+
+
+            o2.randomNumber = TestTools.CreateVector(-5, -5);
+            CheckEquationShowResult(e1, e1.CreateFunction(), "f(x) = ((-5 - x) * x)", new double[] { -24, -36, -50, -66 });
+            
+            o2.randomNumber = TestTools.CreateVector(3, 3);
+            CheckEquationShowResult(e1, e1.CreateFunction(), "f(x) = ((3 - x) * x)", new double[] { 0, -4, -10, -18 });
+
+
+
+            o2.MFunction = new Plus();
+            CheckEquationShowResult(e1, e1.CreateFunction(), "f(x) = ((3 + x) * x)", new double[] { 18, 28, 40, 54 });
+
+            o2.MFunction = new Multiply();
+            CheckEquationShowResult(e1, e1.CreateFunction(), "f(x) = ((3 * x) * x)", new double[] { 27, 48, 75, 108 });
+
+            o2.MFunction = new Divide();
+            CheckEquationShowResult(e1, e1.CreateFunction(), "f(x) = ((3 / x) * x)", new double[] { 3, 3, 3, 3 });
+        }
+
+        [TestMethod]
+        public void TestEquation2()
+        {
+
         }
 
         private void CheckEquationShowResult(Equation e, string equation, string expected, double[] expectedResults)
