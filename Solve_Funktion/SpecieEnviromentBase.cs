8af5631377a4882abb65cc4bddcf48d70f1ad745
TheAIBot <andreasuranus@hotmail.com>
2015-12-20 04:29:12 +0100

    game VectorPoint its own file

--- Solve_Funktion/SpecieEnviromentBase.cs
+++ Solve_Funktion/SpecieEnviromentBase.cs
@@ -84,19 +84,5 @@ namespace Solve_Funktion
         public Genome Specie;
     }
 
-    public sealed class VectorPoint
-    {
-        public Vector<double>[] Parameters;
-        public string[] ParameterNames;
-        public Vector<double> Result;
-        public int Count;
-
-        public VectorPoint(Vector<double>[] parameters, string[] parameterNames, Vector<double> result, int count)
-        {
-            Parameters = parameters;
-            ParameterNames = parameterNames;
-            Result = result;
-            Count = count;
-        }
-    }
+    
 }
