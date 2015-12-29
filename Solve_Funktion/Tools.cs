262d643bae8b85cad466c5bb02e96d168c1e6583
TheAIBot <andreasuranus@hotmail.com>
2015-12-29 18:36:02 +0100

    IsNumber more code reused

--- Solve_Funktion/Tools.cs
+++ Solve_Funktion/Tools.cs
@@ -11,7 +11,7 @@ namespace Solve_Funktion
         {
             for (int i = 0; i < Constants.VECTOR_LENGTH; i++)
             {
-                if (double.IsNaN(ToDecide[i]) || double.IsInfinity(ToDecide[i]))
+                if (IsANumber(ToDecide[i]))
                 {
                     return false;
                 }
